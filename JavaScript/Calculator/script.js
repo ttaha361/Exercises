e555641385bf83a96eaa7eb05d6d2690f8e57730
Taha Ören <taha33oren@gmail.com>
2023-08-08 20:16:42 +0300

    New Calculator Start

--- JavaScript/Calculator/script.js
+++ JavaScript/Calculator/script.js
@@ -8,13 +8,31 @@ const six = document.getElementById('six')
 const seven = document.getElementById('seven')
 const eight = document.getElementById('eight')
 const nine = document.getElementById('nine')
+const result = document.getElementById('result')
 
-let numberone = 0;
-
-zero.addEventListener('click', ()=>{
-    numberMake(zero.textContent);
-})
+let numberone = 0
+let numbertwo = 0
+let oneSelected = false
+let twoSelected = false
 
 function numberMake(numberValue) {
     alert(numberValue);
 }
+
+zero.addEventListener('click', ()=>{
+    if (oneSelected == false) {
+        oneSelected = true
+    }
+    else if (twoSelected == false) {
+        twoSelected = true
+    }
+})
+
+one.addEventListener('click', ()=>{
+    if (oneSelected == false) {
+        oneSelected = true
+    }
+    else if (twoSelected == false) {
+        twoSelected = true
+    }
+})
