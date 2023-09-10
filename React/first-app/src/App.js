2c06f76aa576c34c3d76c3bb0efd3880a8740ce1
Taha Ören <taha33oren@gmail.com>
2023-09-10 12:34:56 +0300

    React 1

--- React/first-app/src/App.js
+++ React/first-app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+// Hello component
+function Hello() {
+  return <div>Hello</div>;
+}
+
+// Message component
+function Message() {
+  return <div>It's time to learn React!!!</div>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello />
+      <Message />
     </div>
   );
 }
