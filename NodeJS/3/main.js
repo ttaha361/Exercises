9f719dadd4a7b5739d744c32d3fc1918366873ee
Taha Ören <taha33oren@gmail.com>
2023-10-22 15:03:15 +0300

    Node 3

---
+++ NodeJS/3/main.js
@@ -0,0 +1,6 @@
+// main.mjs
+// Import the greet function from myModule
+import greet from './myModule.mjs';
+
+// Call the greet function
+greet('Taha');
---
+++ NodeJS/3/myModule.js
@@ -0,0 +1,8 @@
+// myModule.mjs
+const greet = (name) => {
+    console.log(`This is, ${name}!`);
+  };
+  
+  // Export the greet function as default
+  export default greet;
+  
